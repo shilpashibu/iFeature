354907b029f72f9df6a2e56f4c0d19b3a0ac70e0
shilpashibu <86522669+shilpashibu@users.noreply.github.com>
2022-04-17 13:05:16 +0530

    Update CTDC.py

--- codes/CTDC.py
+++ codes/CTDC.py
@@ -46,10 +46,26 @@ def CTDC(fastas, **kw):
 	for i in fastas:
 		name, sequence = i[0], re.sub('-', '', i[1])
 		code = [name]
+		aaPair = [sequence[j:j + 2] for j in range(len(sequence) - 1)]
 		for p in property:
 			c1 = Count(group1[p], sequence) / len(sequence)
 			c2 = Count(group2[p], sequence) / len(sequence)
 			c3 = 1 - c1 - c2
 			code = code + [c1, c2, c3]
 		encodings.append(code)
+		for p in property:
+			c1221, c1331, c2332 = 0, 0, 0
+			for pair in aaPair:
+				if (pair[0] in group1[p] and pair[1] in group2[p]) or (pair[0] in group2[p] and pair[1] in group1[p]):
+					c1221 = c1221 + 1
+					continue
+				if (pair[0] in group1[p] and pair[1] in group3[p]) or (pair[0] in group3[p] and pair[1] in group1[p]):
+					c1331 = c1331 + 1
+					continue
+				if (pair[0] in group2[p] and pair[1] in group3[p]) or (pair[0] in group3[p] and pair[1] in group2[p]):
+					c2332 = c2332 + 1
+			code = code + [c1221/len(aaPair), c1331/len(aaPair), c2332/len(aaPair)]
+		encodings.append(code)
 	return encodings
+
+	
